5da1ce8c524f26396cf1ee71e22a43a09a125268
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-10-07 06:50:20 +0000

    🚑 tailwind config set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
